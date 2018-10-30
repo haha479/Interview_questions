0180a1a93d0e139d98cdeef1c037af585d9153ed
haha479 <283275935@qq.com>
2018-10-30 13:47:43 +0800

    两个队列实现一个栈

--- 31-栈队列互相实现.py
+++ 31-栈队列互相实现.py
@@ -1,4 +1,13 @@
-# 两个栈实现一个队列
+'''
+两个栈实现一个队列
+入队：元素进栈A
+
+出队：先判断栈B是否为空，为空则将栈A中的元素 pop 出来并 push 进栈B，再栈B出栈，如不为空则栈B直接出栈
+
+复杂度分析：
+
+这样用两个栈实现一个队列,入队的复杂度为O(1)，出队的复杂度则变为O(n)。
+'''
 class Queue(object):
 	def __init__(self):
 		self.stack1 = []
@@ -23,3 +32,32 @@ q.push(2)
 
 print(q.pop())
 print(q.pop())
+
+
+'''
+两个队列实现一个栈
+'''
+
+class Stack(object):
+	def __init__(self):
+		self.queue1 = []
+		self.queue2 = []
+
+	def push(self, item):
+		self.queue1.append(item)
+
+	def pop(self):
+		if len(self.queue1) == 0:
+			return None
+		while len(self.queue1) != 1:
+			self.queue2.append(self.queue1.pop(0))
+		self.queue1, self.queue2 = self.queue2, self.queue1
+
+		return self.queue2.pop()
+
+s = Stack()
+s.push('a')
+s.push('b')
+
+print(s.pop())
+print(s.pop())
